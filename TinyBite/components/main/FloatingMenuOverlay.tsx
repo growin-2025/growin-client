ce4da25dd927b778fcd9c516238c0332c3ad5da2
ion <delion.dev@gmail.com>
2025-12-16 15:49:20 +0900

    feat/#44: 플로팅 메뉴 오버레이에 배달, 생필품, 장보기 버튼 UI 구현

--- TinyBite/components/main/FloatingMenuOverlay.tsx
+++ TinyBite/components/main/FloatingMenuOverlay.tsx
@@ -1,4 +1,7 @@
+import { colors } from "@/styles/colors";
+import { textStyles } from "@/styles/typography/textStyles";
 import {
+  Image,
   Modal,
   Pressable,
   StyleSheet,
@@ -24,8 +27,51 @@ const FloatingMenuOverlay = ({
       onRequestClose={onClose}
       statusBarTranslucent={true}
     >
-      <Pressable style={styles.overlay}>
-        <Text>test</Text>
+      <Pressable style={styles.overlay} onPress={onClose}>
+        <View style={styles.menuContainer}>
+          <TouchableOpacity
+            style={styles.menuButton}
+            onPress={() => console.log("배달 클릭")}
+          >
+            <Image
+              style={styles.menuIcon}
+              source={require("@/assets/images/main/category/delivery.png")}
+            />
+            <Text style={[styles.menuText, textStyles.title20_SB135]}>
+              배달
+            </Text>
+          </TouchableOpacity>
+
+          <View style={styles.divider} />
+
+          <TouchableOpacity
+            style={styles.menuButton}
+            onPress={() => console.log("생필품 클릭")}
+          >
+            <Image
+              style={styles.menuIcon}
+              source={require("@/assets/images/main/category/essentials.png")}
+            />
+            <Text style={[styles.menuText, textStyles.title20_SB135]}>
+              생필품
+            </Text>
+          </TouchableOpacity>
+
+          <View style={styles.divider} />
+
+          <TouchableOpacity
+            style={styles.menuButton}
+            onPress={() => console.log("장보기 클릭")}
+          >
+            <Image
+              style={styles.menuIcon}
+              source={require("@/assets/images/main/category/grocery.png")}
+            />
+            <Text style={[styles.menuText, textStyles.title20_SB135]}>
+              장보기
+            </Text>
+          </TouchableOpacity>
+        </View>
       </Pressable>
     </Modal>
   );
@@ -37,6 +83,42 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(34, 34, 34, 0.50)",
     justifyContent: "flex-end",
     alignItems: "flex-end",
+    paddingRight: 20,
+    paddingBottom: 160,
+  },
+
+  menuContainer: {
+    padding: 20,
+    justifyContent: "center",
+    gap: 8,
+    borderRadius: 16,
+    backgroundColor: "#fff",
+    // 그림자 효과 (iOS)
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+    shadowRadius: 16,
+    // 그림자 효과 (Android)
+    elevation: 3,
+  },
+
+  menuButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+    alignSelf: "stretch",
+  },
+  menuIcon: {
+    width: 32,
+    height: 32,
+    aspectRatio: 1 / 1,
+  },
+  menuText: {
+    color: colors.gray[1],
+  },
+
+  divider: {
+    height: 1,
+    backgroundColor: colors.gray[4],
+    paddingHorizontal: 8,
   },
 });
 
