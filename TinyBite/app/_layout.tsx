9f81d81dbafb8bed6bcaf1ceb317df8a5545164c
ion <delion.dev@gmail.com>
2025-12-18 16:36:02 +0900

    feat/#57: React Query를 사용하여 데이터 패칭을 위한 QueryClientProvider 추가

--- TinyBite/app/_layout.tsx
+++ TinyBite/app/_layout.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="main-card-detail" />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="main-card-detail" />
+      </Stack>
+    </QueryClientProvider>
   );
 }
