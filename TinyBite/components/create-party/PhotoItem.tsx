3e9f7cd2a61cd2e62414b00e3ddca52589463d33
ion <delion.dev@gmail.com>
2025-12-21 08:49:15 +0900

    feat/#36: PhotoItem 컴포넌트 추가 및 UI 구현

---
+++ TinyBite/components/create-party/PhotoItem.tsx
@@ -0,0 +1,84 @@
+import { colors } from "@/styles/colors";
+import { textStyles } from "@/styles/typography/textStyles";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const X_MARK_ICON = require("@/assets/images/x-mark-14-gray.png");
+
+interface PhotoItemProps {
+  id: string;
+  url: string;
+}
+
+const PhotoItem = ({ id, url }: PhotoItemProps) => {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: url }} resizeMode="cover" />
+
+        <View style={styles.textContainer}>
+          <Text style={[styles.text, textStyles.body12_M135]}>대표</Text>
+        </View>
+      </View>
+
+      <TouchableOpacity style={styles.xButton}>
+        <Image style={styles.xIcon} source={X_MARK_ICON} />
+      </TouchableOpacity>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "relative",
+    justifyContent: "flex-end",
+    width: 64,
+    height: 64,
+  },
+  imageContainer: {
+    position: "relative",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    width: 60,
+    height: 60,
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  image: {
+    width: 60,
+    height: 60,
+  },
+  textContainer: {
+    position: "absolute",
+    bottom: 4,
+    right: 4,
+    paddingVertical: 2,
+    paddingHorizontal: 4,
+    borderRadius: 100,
+    backgroundColor: colors.main,
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 4,
+    elevation: 4,
+  },
+  text: { color: "#fff" },
+
+  xButton: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    width: 16,
+    height: 16,
+    padding: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: colors.gray[4],
+  },
+  xIcon: {
+    width: 14,
+    height: 14,
+  },
+});
+
+export default PhotoItem;
