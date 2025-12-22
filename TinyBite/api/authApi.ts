375b6c37f9e20366705d0c64dec1a4227bbd9a3c
ion <delion.dev@gmail.com>
2025-12-23 02:30:26 +0900

    feat/#57: SMS 전송 api 추가

--- TinyBite/api/authApi.ts
+++ TinyBite/api/authApi.ts
@@ -11,3 +11,8 @@ export const postSignupGoogle = async (signupData: SignupGoogle) => {
   const res = await publicAxios.post(ENDPOINT.AUTH.SIGNUP_GOOGLE, signupData);
   return res.data;
 };
+
+export const postSmsSend = async ({ phone }: { phone: string }) => {
+  const res = await publicAxios.post(ENDPOINT.AUTH.SIGNUP_GOOGLE, phone);
+  return res.data;
+};
--- TinyBite/api/urls.ts
+++ TinyBite/api/urls.ts
@@ -3,6 +3,9 @@ import { API_URL } from "./configs";
 export const BASE_URL = API_URL;
 
 export const ENDPOINT = {
+  SMS_AUTH: {
+    SMS_SEND: "/api/v1/auth/sms/send",
+  },
   AUTH: {
     SIGNUP_GOOGLE: "/api/v1/auth/google/signup",
     LOGIN_GOOGLE: "/api/v1/auth/google/login",
