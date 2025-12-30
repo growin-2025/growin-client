e80cbd2b411bf5a9c8079b92e783bb5bb85889d1
ParkSeohyeon <tjgus4394@gmail.com>
2025-12-30 18:34:32 +0900

    feat/#68: 파티 리스트 카테고리 필터 기능 추가

--- TinyBite/hooks/usePartyList.ts
+++ TinyBite/hooks/usePartyList.ts
@@ -1,16 +1,24 @@
 import { getPartyList } from "@/api/partyApi";
-import { PartyListParams, PartyListResponse } from "@/types/party";
+import { PartyCategory, PartyListResponse } from "@/types/party";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 /**
  * 파티 리스트 조회 Hook
- * @param params 파티 리스트 조회 파라미터
- * @returns React Query 결과 (data, isLoading, error 등)
+ * @param location 사용자 위치 정보 (latitude, longitude)
+ * @returns React Query 결과 (data, isLoading, error 등) 및 카테고리 상태
  */
-export const usePartyList = (params: PartyListParams) => {
-  return useQuery<PartyListResponse>({
-    queryKey: ["getParties", params.category],
-    queryFn: () => getPartyList(params),
-    enabled: !!params.latitude && !!params.longitude, // latitude와 longitude가 있을 때만 실행
+export const usePartyList = (location: {
+  latitude: string;
+  longitude: string;
+}) => {
+  const [partyType, setPartyType] = useState<PartyCategory>("ALL");
+
+  const { data, isLoading, error } = useQuery<PartyListResponse>({
+    queryKey: ["getParties", partyType, location.latitude, location.longitude],
+    queryFn: () => getPartyList({ category: partyType, ...location }),
+    enabled: !!location.latitude && !!location.longitude, // latitude와 longitude가 있을 때만 실행
   });
+
+  return { data, isLoading, error, partyType, setPartyType };
 };
