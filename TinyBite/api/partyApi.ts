ceb558ce6f2a9fe6facf2cf34618fac4e5991074
ParkSeohyeon <tjgus4394@gmail.com>
2025-12-30 15:56:25 +0900

    chore/#68: 파티 리스트 임시 데이터 추가

--- TinyBite/api/partyApi.ts
+++ TinyBite/api/partyApi.ts
@@ -1,5 +1,3 @@
-import { privateAxios } from "@/api/axios";
-import { ENDPOINT } from "@/api/urls";
 import { PartyListParams, PartyListResponse } from "@/types/party";
 
 /**
@@ -10,7 +8,8 @@ import { PartyListParams, PartyListResponse } from "@/types/party";
 export const getPartyList = async (
   params: PartyListParams
 ): Promise<PartyListResponse> => {
-  const res = await privateAxios.get(ENDPOINT.PARTY.GET_PARTIES, {
+  /** 
+    const res = await privateAxios.get(ENDPOINT.PARTY.GET_PARTIES, {
     params: {
       category: params.category,
       latitude: params.latitude,
@@ -18,4 +17,55 @@ export const getPartyList = async (
     },
   });
   return res.data.data;
+  */
+
+  // 데이터가 없을 때 임시로 가짜 데이터 반환
+  return {
+    activeParties: [
+      {
+        partyId: 1,
+        thumbnailImage:
+          "https://cdn.imweb.me/thumbnail/20230228/25687782da912.png",
+        title: "뿌링클 같이 나눠드실 분!",
+        pricePerPerson: 5000,
+        participantStatus: "1/4명",
+        distance: "500m 이내",
+        distanceKm: 0.5,
+        timeAgo: "방금 전",
+        isClosed: false,
+        category: "DELIVERY",
+        createdAt: "2025-12-30T05:59:56.447Z",
+      },
+      {
+        partyId: 3,
+        thumbnailImage: "https://picsum.photos/200",
+        title: "휴지 30개 나눔해염",
+        pricePerPerson: 5000,
+        participantStatus: "1/4명",
+        distance: "500m 이내",
+        distanceKm: 0.5,
+        timeAgo: "방금 전",
+        isClosed: false,
+        category: "GROCERY",
+        createdAt: "2025-12-30T05:59:56.447Z",
+      },
+    ],
+    closedParties: [
+      {
+        partyId: 2,
+        thumbnailImage: "https://picsum.photos/200",
+        title: "마감이여 뿌링클 같이 나눠드실 분! 뿌링클 같이 나눠드실 분!",
+        pricePerPerson: 5000,
+        participantStatus: "3/4명",
+        distance: "500m 이내",
+        distanceKm: 0.5,
+        timeAgo: "방금 전",
+        isClosed: true,
+        category: "HOUSEHOLD",
+        createdAt: "2025-12-30T05:59:56.447Z",
+      },
+    ],
+    hasNext: false,
+    totalCount: 1,
+  };
 };
