2fce50d7fdce084da0de3230fc1544ac77e627df
ParkSeohyeon <tjgus4394@gmail.com>
2026-01-02 22:09:29 +0900

    style/#84: 파티리스트 UI 수정

--- TinyBite/components/main/MainCard.tsx
+++ TinyBite/components/main/MainCard.tsx
@@ -105,7 +105,8 @@ const MainCard = ({ item, onPress, containerStyle }: MainCardProps) => {
             </Text>
           </View>
           <Text style={[styles.meta, textStyles.body13_SB135]}>
-            {item.distance} | {item.timeAgo}
+            {/* {item.distance}  |*/}
+            {item.timeAgo}
           </Text>
         </View>
       </View>
