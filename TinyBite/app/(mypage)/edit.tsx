fd102e7c0b39e11ce7f1afe0d28a594404d7c0a5
ParkSeohyeon <tjgus4394@gmail.com>
2026-01-03 00:56:27 +0900

    chore/#84: import 공백제거

--- TinyBite/app/(mypage)/edit.tsx
+++ TinyBite/app/(mypage)/edit.tsx
@@ -2,7 +2,7 @@ import { checkNickname, getUserMe, updateNickname } from "@/api/userApi";
 import { colors } from "@/styles/colors";
 import { textStyles } from "@/styles/typography/textStyles";
 import { ApiError } from "@/types/api";
-import { getErrorMessage } from "@/utils/getErrorMessage ";
+import { getErrorMessage } from "@/utils/getErrorMessage";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { useRouter } from "expo-router";
--- TinyBite/app/(mypage)/neighborhood.tsx
+++ TinyBite/app/(mypage)/neighborhood.tsx
@@ -3,7 +3,7 @@ import { useUserCoords } from "@/hooks/useUserCoords";
 import { colors } from "@/styles/colors";
 import { textStyles } from "@/styles/typography/textStyles";
 import { ApiError } from "@/types/api";
-import { getErrorMessage } from "@/utils/getErrorMessage ";
+import { getErrorMessage } from "@/utils/getErrorMessage";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { useRouter } from "expo-router";
