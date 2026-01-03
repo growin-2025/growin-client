47d5b457e2e63fc7988bb57b5143568aef4da6f8
ion <delion.dev@gmail.com>
2026-01-03 10:13:00 +0900

    feat: 버전 0.2.0으로 업데이트

--- TinyBite/app.config.js
+++ TinyBite/app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "한입만",
   slug: "TinyBite",
   owner: "tinybite-2025",
-  version: "0.1.0",
+  version: "0.2.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "tinybite",
