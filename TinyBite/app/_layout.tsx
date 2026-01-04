d8f90d88c80878bff487a8ea437d8946e709bff0
ion <delion.dev@gmail.com>
2026-01-04 23:47:58 +0900

    fix/#94:  키보드 노출 시 화면이 고정되어 가려지는 문제 해결

--- TinyBite/app/_layout.tsx
+++ TinyBite/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { toastConfig } from "@/lib/toast/toastConfig";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { KeyboardProvider } from "react-native-keyboard-controller";
 import Toast from "react-native-toast-message";
 
 const queryClient = new QueryClient();
@@ -8,12 +9,14 @@ const queryClient = new QueryClient();
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="party-detail/[id]" />
-      </Stack>
-      <Toast config={toastConfig} />
+      <KeyboardProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="party-detail/[id]" />
+        </Stack>
+        <Toast config={toastConfig} />
+      </KeyboardProvider>
     </QueryClientProvider>
   );
 }
--- TinyBite/app/party/create/[type].tsx
+++ TinyBite/app/party/create/[type].tsx
@@ -16,6 +16,7 @@ import { AxiosError } from "axios";
 import { router, useLocalSearchParams } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { FlatList, ScrollView, StyleSheet, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-controller";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
 import { useShallow } from "zustand/shallow";
@@ -196,89 +197,94 @@ export default function PartyCreateScreen() {
       <View style={styles.container}>
         <CreatePartyPageHeader title={title} />
 
-        <ScrollView style={styles.contentContainer}>
-          <SafeAreaView style={styles.contentInner} edges={["bottom"]}>
-            <View style={styles.section}>
-              <SubTitle subTitle="사진 등록" isPic />
-              <View style={styles.photoContainer}>
-                <AddPhotoButton />
-                <FlatList
-                  data={photos}
-                  renderItem={renderItem}
-                  keyExtractor={(item) => item.id.toString()}
-                  horizontal={true}
-                  contentContainerStyle={{ gap: 8 }}
-                />
-              </View>
-            </View>
-
-            <View style={styles.section}>
-              <SubTitle subTitle="파티 제목" caption="(메뉴명)" />
-              <TextInputBox
-                placeholder={config.titlePlaceholder}
-                maxLength={30}
-                onChangeText={setPartyTitle}
-                value={partyTitle}
-              />
-            </View>
-
-            <View style={styles.section}>
-              <SubTitle subTitle="예상 총 주문 금액" />
-              <TextInputBox
-                placeholder="0"
-                isAmount
-                onChangeText={setTotalAmount}
-                value={totalAmount}
-              />
-            </View>
-
-            <View style={styles.section}>
-              <SubTitle subTitle="모집 인원" caption="(나 포함)" />
-              <NumberOfPeopleBox />
-            </View>
-
-            <View style={styles.section}>
-              <SubTitle subTitle="수령 장소" />
-              <TextInputBox
-                iconType="location"
-                placeholder="예) 역삼역 1번 출구"
-                maxLength={30}
-                onChangeText={setPickUpLocation}
-                value={pickUpLocation.place}
-              />
-            </View>
-
-            <View style={styles.section}>
-              <SubTitle subTitle="상세 설명" />
-              <TextInputBox
-                placeholder="추가로 전달 할 내용이 있다면 적어주세요."
-                maxLength={60}
-                onChangeText={setDetailedDescription}
-                value={detailedDescription}
-              />
-            </View>
-
-            {config.showProductLink && (
+        <KeyboardAwareScrollView contentContainerStyle={{ flex: 1 }}>
+          <ScrollView style={styles.contentContainer}>
+            <View style={styles.contentInner}>
               <View style={styles.section}>
-                <SubTitle subTitle="상품 링크" />
+                <SubTitle subTitle="사진 등록" isPic />
+                <View style={styles.photoContainer}>
+                  <AddPhotoButton />
+                  <FlatList
+                    data={photos}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id.toString()}
+                    horizontal={true}
+                    contentContainerStyle={{ gap: 8 }}
+                  />
+                </View>
+              </View>
+
+              <View style={styles.section}>
+                <SubTitle subTitle="파티 제목" caption="(메뉴명)" />
                 <TextInputBox
-                  iconType="link"
-                  placeholder="구매할 상품의 URL을 입력하세요."
-                  onChangeText={setProductLink}
-                  value={productLink}
+                  placeholder={config.titlePlaceholder}
+                  maxLength={30}
+                  onChangeText={setPartyTitle}
+                  value={partyTitle}
                 />
               </View>
-            )}
-          </SafeAreaView>
-        </ScrollView>
 
-        <SafeAreaView style={styles.createButtonContainer} edges={["bottom"]}>
-          <GlobalButton
-            onClick={onClickCreateParty}
-            text="파티 시작하기"
-            disabled={!isValid()}
-          />
-        </SafeAreaView>
+              <View style={styles.section}>
+                <SubTitle subTitle="예상 총 주문 금액" />
+                <TextInputBox
+                  placeholder="0"
+                  isAmount
+                  onChangeText={setTotalAmount}
+                  value={totalAmount}
+                />
+              </View>
+
+              <View style={styles.section}>
+                <SubTitle subTitle="모집 인원" caption="(나 포함)" />
+                <NumberOfPeopleBox />
+              </View>
+
+              <View style={styles.section}>
+                <SubTitle subTitle="수령 장소" />
+                <TextInputBox
+                  iconType="location"
+                  placeholder="예) 역삼역 1번 출구"
+                  maxLength={30}
+                  onChangeText={setPickUpLocation}
+                  value={pickUpLocation.place}
+                />
+              </View>
+
+              <View style={styles.section}>
+                <SubTitle subTitle="상세 설명" />
+                <TextInputBox
+                  placeholder="추가로 전달 할 내용이 있다면 적어주세요."
+                  maxLength={60}
+                  onChangeText={setDetailedDescription}
+                  value={detailedDescription}
+                />
+              </View>
+
+              {config.showProductLink && (
+                <View style={styles.section}>
+                  <SubTitle subTitle="상품 링크" />
+                  <TextInputBox
+                    iconType="link"
+                    placeholder="구매할 상품의 URL을 입력하세요."
+                    onChangeText={setProductLink}
+                    value={productLink}
+                  />
+                </View>
+              )}
+            </View>
+
+            <SafeAreaView
+              style={styles.createButtonContainer}
+              edges={["bottom"]}
+            >
+              <GlobalButton
+                onClick={onClickCreateParty}
+                text="파티 시작하기"
+                disabled={!isValid()}
+              />
+            </SafeAreaView>
+          </ScrollView>
+        </KeyboardAwareScrollView>
       </View>
     </>
   );
