637869d518e6f6615a7fc20d9385e5cc33c07f53
ion <delion.dev@gmail.com>
2026-01-10 02:03:32 +0900

    feat/#71: 채팅 입력 박스 UI 구현

---
+++ TinyBite/components/chat/ChatInputBox.tsx
@@ -0,0 +1,80 @@
+import { colors } from "@/styles/colors";
+import { textStyles } from "@/styles/typography/textStyles";
+import { useCallback, useState } from "react";
+import { Image, StyleSheet, TextInput, View } from "react-native";
+
+const PLUS_ICON = require("@/assets/images/plus-32-gray.png");
+const SEND_ICON = require("@/assets/images/chat/send.png");
+
+const ChatInputBox = () => {
+  const [message, setMessage] = useState("");
+
+  const handleMessageChange = useCallback((text: string) => {
+    setMessage(text);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerWrapper}>
+        <Image style={styles.iconPlus} source={PLUS_ICON} />
+
+        <View style={styles.inputWrapper}>
+          <TextInput
+            style={[styles.inputArea, textStyles.body16_SB135]}
+            placeholder="메시지를 입력하세요"
+            placeholderTextColor={colors.gray[2]}
+            multiline={true}
+            keyboardType={"default"}
+            onChangeText={handleMessageChange}
+            value={message}
+            numberOfLines={3}
+          />
+        </View>
+
+        <Image style={styles.iconSend} source={SEND_ICON} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    paddingTop: 12,
+    paddingBottom: 11,
+    paddingHorizontal: 20,
+  },
+  containerWrapper: {
+    alignItems: "center",
+    gap: 8,
+    flexDirection: "row",
+  },
+
+  iconPlus: {
+    width: 32,
+    height: 32,
+  },
+  iconSend: {
+    width: 28,
+    height: 28,
+  },
+
+  inputWrapper: {
+    flexGrow: 1,
+    padding: 12,
+    borderRadius: 16,
+    backgroundColor: colors.background,
+    shadowColor: "rgba(0,0,0,0.25)",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 8,
+    elevation: 4,
+  },
+  inputArea: {
+    justifyContent: "center",
+    color: colors.black,
+    padding: 0,
+  },
+});
+
+export default ChatInputBox;
