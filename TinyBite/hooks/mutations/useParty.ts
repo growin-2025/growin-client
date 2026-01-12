f31e9cd6e12ae0052da76c7dee38d0bee9413791
ion <delion.dev@gmail.com>
2026-01-12 16:17:18 +0900

    feat, refactor/#108: 파일 업로드 및 파티 참여 요청 기능 추가, 채팅방 목록 조회 훅 추가

---
+++ TinyBite/hooks/mutations/useParty.ts
@@ -0,0 +1,43 @@
+import { postRequestJoinParty } from "@/api/partyApi";
+import { ApiError } from "@/types/api.types";
+import { getErrorMessage } from "@/utils/getErrorMessage";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "expo-router";
+import Toast from "react-native-toast-message";
+
+export const useRequestJoinParty = () => {
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: postRequestJoinParty,
+    onSuccess: (data) => {
+      router.push({
+        pathname: "/chat/[id]",
+        params: {
+          id: data,
+          roomType: "ONE_TO_ONE",
+        },
+      });
+    },
+    onError: (error: AxiosError<ApiError>) => {
+      if (error.status === 400) {
+        Toast.show({
+          type: "basicToast",
+          props: { text: "이미 참여 요청을 보낸 파티입니다." },
+          position: "bottom",
+          bottomOffset: 133,
+          visibilityTime: 2000,
+        });
+        return;
+      }
+      if (error.response?.data) {
+        const message = getErrorMessage(error.response.data);
+        alert(message);
+        console.error(message);
+      } else {
+        console.error("네트워크 연결을 확인해주세요.");
+      }
+    },
+  });
+};
