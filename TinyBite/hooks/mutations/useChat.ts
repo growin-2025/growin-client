ed3c55ddb8b80e071db17d276bd1aa0a9456d54b
ion <delion.dev@gmail.com>
2026-01-16 00:19:16 +0900

    feat/#115: 그룹 관련 파티 완료 및 정산 로직 추가

--- TinyBite/hooks/mutations/useChat.ts
+++ TinyBite/hooks/mutations/useChat.ts
@@ -1,4 +1,9 @@
-import { postApproveJoinParty, postRejectJoinParty } from "@/api/partyApi";
+import {
+  postApproveJoinParty,
+  postCompleteParty,
+  postRejectJoinParty,
+  postSettleParty,
+} from "@/api/partyApi";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import Toast from "react-native-toast-message";
 
@@ -58,6 +63,53 @@ export const useRejectJoinPartyMutation = (
       Toast.show({
         type: "basicToast",
         props: { text: "파티 참여 거절에 실패했습니다. 다시 시도해주세요." },
+        position: "bottom",
+        bottomOffset: 133,
+        visibilityTime: 2000,
+      });
+    },
+  });
+};
+
+export const useCompletePartyMutation = (
+  partyId: number,
+  chatroomId: number
+) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: () => postCompleteParty(partyId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["getOnetoOneRoomDetail", chatroomId],
+      });
+    },
+    onError: (error) => {
+      Toast.show({
+        type: "basicToast",
+        props: { text: error.message },
+        position: "bottom",
+        bottomOffset: 133,
+        visibilityTime: 2000,
+      });
+    },
+  });
+};
+
+export const useSettlePartyMutation = (partyId: number, chatroomId: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: () => postSettleParty(partyId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["getOnetoOneRoomDetail", chatroomId],
+      });
+    },
+    onError: (error) => {
+      Toast.show({
+        type: "basicToast",
+        props: { text: error.message },
         position: "bottom",
         bottomOffset: 133,
         visibilityTime: 2000,
